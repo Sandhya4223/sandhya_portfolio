b1a89f4864e2d0f43f64675d5f707e7a9ffd1e27
Sandhya Kumari <sandhya25297@gmail.com>
2026-09-18 21:46:09 +0530

    Comment

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -45,6 +45,7 @@ const Contact = () => {
         );
     };
 
+    
     return (
         <section id="contact" className="flex flex-col items-center justify-center py-24 px-[12vw] md:px-[7vw] lg:px-[16vw]">
             {/* Toast Container */}
